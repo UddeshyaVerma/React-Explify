dfcd11fade007dd33c690fd9155f517252cb4346
UddeshyaVerma <vuddeshya11@gmail.com>
2020-08-09 21:44:42 +0530

    Updates

---
+++ precache-manifest.e5d43cad200e2ae9ef8a0ecddb0e534b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc26f3cbf89f0382ee5ec779f43f8a4",
+    "url": "/React-Explify/index.html"
+  },
+  {
+    "revision": "29a517325ea010e16eae",
+    "url": "/React-Explify/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2d59b26685426296e5ff",
+    "url": "/React-Explify/static/css/main.41c223ff.chunk.css"
+  },
+  {
+    "revision": "29a517325ea010e16eae",
+    "url": "/React-Explify/static/js/2.e68715b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Explify/static/js/2.e68715b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d59b26685426296e5ff",
+    "url": "/React-Explify/static/js/main.3220c8a6.chunk.js"
+  },
+  {
+    "revision": "c0d9f7c1905e583adfcd",
+    "url": "/React-Explify/static/js/runtime-main.75d18a59.js"
+  },
+  {
+    "revision": "63d4bdb04baf7a375b49a637ea961145",
+    "url": "/React-Explify/static/media/about-img.63d4bdb0.png"
+  },
+  {
+    "revision": "b79a234f0a68c2821df51f7841fd0d2b",
+    "url": "/React-Explify/static/media/bg.b79a234f.png"
+  },
+  {
+    "revision": "fa4407f7ae5fb8b8aeaab0b679c5c60d",
+    "url": "/React-Explify/static/media/features-1.fa4407f7.svg"
+  },
+  {
+    "revision": "b8fad32a02dd9d2bfca4be14fc39f957",
+    "url": "/React-Explify/static/media/features-2.b8fad32a.svg"
+  },
+  {
+    "revision": "8f338c7b1bf7e4e736c7173228849b8a",
+    "url": "/React-Explify/static/media/intro-bg.8f338c7b.jpg"
+  },
+  {
+    "revision": "162cfa5bfd1493b7686f8526c1ecec1b",
+    "url": "/React-Explify/static/media/testimonial-1.162cfa5b.jpg"
+  }
+]);
